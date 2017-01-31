27ffd2fbb3b6174b0941f1a0d51df663749b0413
EltonLaw <eltonlaw296@gmail.com>
2017-01-30 23:19:00 -0500

    Added method for mean imputation

--- basic_imputations.py
+++ basic_imputations.py
@@ -23,6 +23,26 @@ def simple_random_imputation(data):
     filled_data = df.as_matrix()
     return filled_data
 
+def mean_imputation(data):
+    """Multivariate Imputation by Chained Equations
+
+    PARAMETERS
+    ---------
+    data: numpy.ndarray
+
+    RETURNS
+    ------
+    numpy.ndarray
+    
+    """
+    null_xy = np.argwhere(np.isnan(data))
+    cols_missing = set([y  for x,y in null_xy])
+    for x,y in null_xy: 
+        col_wo_nan = data[:,[y]][~np.isnan(data[:,[y]])]
+        new_value = np.mean(col_wo_nan)
+        data[x][y] = new_value
+    return data
+
 def complete_case(data):
     """ Return only data rows with complete data
 
