44d68187c3733152cd9824d78c9245431642e5ff
EltonLaw <eltonlaw296@gmail.com>
2017-02-02 23:08:25 -0500

    Fixed docstring description

--- impy/basic/mean_imputation.py
+++ impy/basic/mean_imputation.py
@@ -1,8 +1,7 @@
-import pandas as pd
 import numpy as np
 
 def mean_imputation(data):
-    """Multivariate Imputation by Chained Equations
+    """ Substitute missing values with the mean of that column
 
     PARAMETERS
     ---------
@@ -13,11 +12,12 @@ def mean_imputation(data):
     numpy.ndarray
     
     """
+    #temp = data
+    data = temp
     null_xy = np.argwhere(np.isnan(data))
     cols_missing = set([y  for x,y in null_xy])
     for x,y in null_xy: 
-        col_wo_nan = data[:,[y]][~np.isnan(data[:,[y]])]
-        new_value = np.mean(col_wo_nan)
+        row_wo_nan= data[:,[y]][~np.isnan(data[:,[y]])]
+        new_value = np.mean(row_wo_nan)
         data[x][y] = new_value
     return data
-
