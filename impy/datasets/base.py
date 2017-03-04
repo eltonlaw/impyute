4853d8e1594ee6f7bdc8a8e7410d15d69c686065
EltonLaw <eltonlaw296@gmail.com>
2017-03-03 22:47:26 -0500

    fixed .data bug

--- impy/datasets/base.py
+++ impy/datasets/base.py
@@ -1,9 +1,10 @@
 " Dataset Generation "
 
 import numpy as np
-from impy.datasets import Mutator
+from impy.datasets.mutate import Mutator
 
-def randomly_generate(range_i=7, data_shape=(5, 5), m_method="MCAR", m_th=0.3):
+
+def randomly_generate(range_i=7, data_shape=(5, 5), m_method="mcar", m_th=0.3):
     """ Return randomly generated data
 
     PARAMETERS
@@ -22,5 +23,5 @@ def randomly_generate(range_i=7, data_shape=(5, 5), m_method="MCAR", m_th=0.3):
     generated_data = np.random.choice(possible_points, data_shape)
     mutator = Mutator()
     mutate = getattr(mutator, m_method)
-    raw_data = mutate(generated_data, threshold=m_th).data
+    raw_data = mutate(generated_data, threshold=m_th)["data"]
     return raw_data
