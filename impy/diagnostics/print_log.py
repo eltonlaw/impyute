90eba3b64d712073a632698e77df2f7e7ec84b42
EltonLaw <eltonlaw296@gmail.com>
2017-03-04 01:29:32 -0500

    add logging fn

---
+++ impy/diagnostics/print_log.py
@@ -0,0 +1,25 @@
+"""Print input/output multiple times"""
+
+
+def print_io(fn, loops=5, **kwargs):
+    """ Prints out input data and output data
+    PARAMETERS
+    ---------
+    fn: Function
+    loops: # of Loops
+    **kwargs: Arguments for random_int function
+
+    RETURNS
+    ------
+    n/a
+
+    """
+    from impy.datasets import random_int
+
+    print("Function:{}".format(str(fn.__name__)))
+    for i in range(loops):
+        data = random_int(**kwargs)
+        print("\n")
+        print("{} ========".format(i))
+        print("{}  {}".format(data, fn(data)))
+    print("============================")
