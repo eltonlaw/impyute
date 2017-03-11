78b6b7baa1302b6de92dee49b9f2861e0bfc59c3
EltonLaw <eltonlaw296@gmail.com>
2017-03-11 03:44:12 -0500

    change theme

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,5 @@
+# NOQA
+# flake8: noqa
 #pylint: skip-file
 #
 # impy documentation build configuration file, created by
@@ -115,7 +117,7 @@ todo_include_todos = True
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'alabaster'
+html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
