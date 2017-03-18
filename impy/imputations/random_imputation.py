2de244efc20d1dab16e7467455fef4294d076f3d
EltonLaw <eltonlaw296@gmail.com>
2017-03-18 14:14:23 -0400

    remove dependency on pandas

--- impy/imputations/random_imputation.py
+++ impy/imputations/random_imputation.py
@@ -3,7 +3,7 @@ Simple Random Imputation
 -----------------------
 """
 import numpy as np
-import pandas as pd
+from impy.diagnostics import find_null
 
 
 def random_imputation(data):
@@ -17,15 +17,9 @@ def random_imputation(data):
     ------
     numpy.ndarray
     """
-    df = pd.DataFrame(data)
-
-    for col in df:
-        df_null = pd.isnull(df[col])
-        if True in df_null.values:
-            # .unique method includes null values so we have to drop it
-            unique_values = [a_i for a_i in df[col].unique()
-                             if not(a_i == ([] or 0 or None)) and
-                             not np.isnan(a_i)]
-            df[col][df_null] = np.random.choice(unique_values)
-    filled_data = df.as_matrix()
-    return filled_data
+    null_xy = find_null(data)
+    for x, y in null_xy:
+        uniques = np.unique(data[:, y])
+        uniques = uniques[~np.isnan(uniques)]
+        data[x][y] = np.random.choice(uniques)
+    return data
