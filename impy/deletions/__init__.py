8b788765ed21f4d8a7636679f7bf49cd3ce146c5
EltonLaw <eltonlaw296@gmail.com>
2017-04-03 16:59:55 -0400

    add __init__.py

---
+++ impy/deletions/__init__.py
@@ -0,0 +1,7 @@
+"""
+The :mod: impy.deletions module implements deletion algorithms
+"""
+
+from .complete_case import complete_case
+
+__all__ = ["complete_case"]
---
+++ impy/tools/__init__.py
@@ -0,0 +1,6 @@
+"""
+The :mod: impy.tools module provides some simple functions that are shared
+among imputations
+"""
+
+__all__ = ["theta"]
