baa3d53a51f09897bdba5bc0b170a784d3e902c9
EltonLaw <eltonlaw296@gmail.com>
2017-04-05 22:28:54 -0400

    add fn to create test data

--- impy/datasets/base.py
+++ impy/datasets/base.py
@@ -6,7 +6,7 @@ from impy.datasets.mutate import Mutator
 
 
 def random_uniform(bound=(0, 10), shape=(5, 5), missingness="mcar",
-                   threshold=0.2, data_type="int"):
+                   th=0.2, dtype="int"):
     """ Return randomly generated dataset of numbers with uniformly
     distributed values between bound[0] and bound[1]
 
@@ -20,9 +20,9 @@ def random_uniform(bound=(0, 10), shape=(5, 5), missingness="mcar",
         Size of the randomly generated data
     missingness: ('mcar', 'mar', 'mnar')
         Type of missigness you want in your dataset
-    threshold: float between [0,1]
+    th: float between [0,1]
         Percentage of missing data in generated data
-    data_type: ('int','float')
+    dtype: ('int','float')
         Type of data
 
     RETURNS
@@ -31,17 +31,17 @@ def random_uniform(bound=(0, 10), shape=(5, 5), missingness="mcar",
     """
     a = bound[0]
     b = bound[1]
-    if data_type == "int":
-        generated_data = np.random.randint(a, b, size=shape).astype(float)
-    elif data_type == "float":
-        generated_data = np.random.uniform(a, b, size=shape)
-    mutator = Mutator(generated_data)
+    if dtype == "int":
+        data = np.random.randint(a, b, size=shape).astype(float)
+    elif dtype == "float":
+        data = np.random.uniform(a, b, size=shape)
+    mutator = Mutator(data, th=th)
     raw_data = getattr(mutator, missingness)()["data"]
     return raw_data
 
 
 def random_normal(theta=(0, 1), shape=(5, 5), missingness="mcar",
-                  threshold=0.2, data_type="int"):
+                  th=0.2, dtype="int"):
     """ Return randomly generated dataset of numbers with normally
     distributed values with given and sigma.
 
@@ -53,9 +53,9 @@ def random_normal(theta=(0, 1), shape=(5, 5), missingness="mcar",
         Size of the randomly generated data
     missingness: ('mcar', 'mar', 'mnar')
         Type of missigness you want in your dataset
-    threshold: float between [0,1]
+    th: float between [0,1]
         Percentage of missing data in generated data
-    data_type: ('int','float')
+    dtype: ('int','float')
         Type of data
 
     RETURNS
@@ -63,11 +63,32 @@ def random_normal(theta=(0, 1), shape=(5, 5), missingness="mcar",
     numpy.ndarray
     """
     mu, sigma = theta
-    generated_data = np.random.normal(mu, sigma, size=shape)
-    if data_type == "int":
-        generated_data = np.round(generated_data)
-    elif data_type == "float":
+    data = np.random.normal(mu, sigma, size=shape)
+    if dtype == "int":
+        data = np.round(data)
+    elif dtype == "float":
         pass
-    mutator = Mutator()
+    mutator = Mutator(data, th=th)
     raw_data = getattr(mutator, missingness)()["data"]
     return raw_data
+
+
+def test_data(shape=(3, 3), mask=None, th=0.2):
+    """
+    Returns a dataset to use with tests
+    shape:tuple(optional)
+        Size of the generated dataset
+    mask: True/False array, same size as dataset, which
+        Use True where missing values should occur and False everywhere else
+    th: float between[0,1]
+        Percentage of missing data in generated dataset
+    """
+    data = np.reshape(np.arange(shape[0] * shape[1]), shape).astype("float")
+    if mask is None:
+        return data
+    elif np.shape(mask) == shape:
+        np.isnan(data)
+        data[mask] = np.nan
+        return data
+    else:
+        raise ValueError
