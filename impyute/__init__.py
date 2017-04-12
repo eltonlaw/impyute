44ca3de545a06597b54196be089ac2ae3a0a031a
EltonLaw <eltonlaw296@gmail.com>
2017-04-12 11:31:29 -0400

    add variable dunders

--- impyute/__init__.py
+++ impyute/__init__.py
@@ -1,2 +1,9 @@
+__title__ = 'impyute'
+__version__ = '0.01'
+__build__ = 0x021300
+__author__ = 'Elton Law'
+__license__ = 'GPL-3.0'
+__copyright__ = 'Copyright 2017 Elton law'
+
 __all__ = ["datasets", "imputations", "diagnostics",
-           "deletions", "machine_learning"]
+           "deletions", "filter"]
