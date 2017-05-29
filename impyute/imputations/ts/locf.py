3c515356c02a8044d138af7324744c97eb39b143
EltonLaw <eltonlaw296@gmail.com>
2017-05-29 16:48:10 -0400

    make locf more robust

--- impyute/imputations/ts/locf.py
+++ impyute/imputations/ts/locf.py
@@ -4,14 +4,17 @@ Last Observation Carried Forward Algorithm
 """
 
 from impyute.utils import find_null
+import numpy as np
 
 
 def locf(data):
     """ For missing values use the last observation carried forward
 
-    For each set of missing indices, use the value of one column before(same
-    row). In the case that the missing value is the first column, we take one
-    column ahead instead (same row still).
+    For each set of missing indices, use the value of one row before(same
+    column). In the case that the missing value is the first row, look one
+    row ahead instead. If this next row is also NaN, look to the next row.
+    Repeat until you find a row in this column that's not NaN. All the rows
+    before will be filled with this value.
 
     PARAMETERS
     ---------
@@ -23,8 +26,21 @@ def locf(data):
     """
     null_xy = find_null(data)
     for x_i, y_i in null_xy:
-        try:
-            data[x_i][y_i] = data[x_i][y_i-1]
-        except:
-            data[x_i][y_i] = data[x_i][y_i+1]
+        # Simplest scenario, look one row back
+        if x_i-1 > -1:
+            data[x_i][y_i] = data[x_i-1][y_i]
+        # Look n rows forward
+        else:
+            x_residuals = np.shape(data)[0]-x_i-1  # n datapoints left
+            val_found = False
+            for i in range(1, x_residuals):
+                if not np.isnan(data[x_i+i][y_i]):
+                    val_found = True
+                    break
+            if val_found:
+                for x_nan in range(i):
+                    data[x_i+x_nan][y_i] = data[x_i+i][y_i]
+            else:
+                print("Error: Entire Column is NaN")
+                raise Exception
     return data
--- test/imputations/ts/test_locf.py
+++ test/imputations/ts/test_locf.py
@@ -33,25 +33,25 @@ class TestLOCF(unittest.TestCase):
         self.assertTrue(isinstance(imputed, np.ndarray))
 
     def test_na_at_i_start(self):
-        """Check if null value at a row[i],column[0]=row[i],column[1]"""
-        self.data_c[3][0] = np.nan
+        """Check if null value at a row[0],column[0]=row[1],column[0]"""
+        self.data_c[0][0] = np.nan
         actual = locf(self.data_c)
-        self.data_c[3][0] = self.data_c[3][1]
+        self.data_c[0][0] = self.data_c[1][0]
         self.assertTrue(np.array_equal(actual, self.data_c))
 
     def test_na_at_i(self):
-        """Check if a null row[i],column[j]=row[i],column[j+1]"""
+        """Check if a null row[i],column[j]=row[i-1],column[j]"""
         self.data_c[3][3] = np.nan
         actual = locf(self.data_c)
-        self.data_c[3][3] = self.data_c[3][4]
+        self.data_c[3][3] = self.data_c[2][3]
         self.assertTrue(np.array_equal(actual, self.data_c))
 
     def test_na_at_i_end(self):
-        """Check if at row[i],column[last_i]=row[i],column[last-1]"""
-        last_i = len(self.data_c[0]) - 1
-        self.data_c[3][last_i] = np.nan
+        """Check if at row[last_i],column[i]=row[last_i-1],column[i]"""
+        last_i = np.shape(self.data_c)[0]-1
+        self.data_c[last_i][3] = np.nan
         actual = locf(self.data_c)
-        self.data_c[3][last_i] = self.data_c[3][last_i-1]
+        self.data_c[last_i][3] = self.data_c[last_i-1][3]
         self.assertTrue(np.array_equal(actual, self.data_c))
 
 
