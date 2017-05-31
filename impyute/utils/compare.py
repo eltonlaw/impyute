86f2265abd1412998d9fa690d3e8173b5a50b734
EltonLaw <eltonlaw296@gmail.com>
2017-05-31 01:10:27 -0400

    add function to compare different imputation algorithms

--- impyute/utils/compare.py
+++ impyute/utils/compare.py
@@ -1,20 +1,42 @@
 """impyute.utils.compare.py"""
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 
 
-class Tester:
-    """Automates testing of datasets, machine learning models and imputation
-    algorithms"""
-    def __init__(self):
-        """ Initializes the Tester object
+def compare(imputed, log_path="results.txt"):
+    """
+    PARAMETERS
+    ----------
+    imputed: [(str, np.ndarray), (str, np.ndarray)...]
+       List of tuples containing (imputation algorithm name, imputed data)
+    RETURNS
+    ------
+    .txt file of classification results on imputed data
+    """
+    clfs = [["SVC", SVC()],
+            ["KNeighbours", KNeighborsClassifier(2)],
+            ["GaussianNB", GaussianNB()],
+            ["RandomForestClassifier", RandomForestClassifier()]]
 
-        """
-        pass
+    results = {imputation_name: [] for imputation_name, _ in imputed}
 
-    def add(self, key, value):
-        pass
+    for imputation_name, data in imputed:
+        X, y = data
+        X_train, X_test, y_train, y_test = train_test_split(X, y,
+                                                            test_size=0.33,
+                                                            random_state=42)
+        print(imputation_name)
+        for clf_name, clf in clfs:
+            clf.fit(X_train, y_train)
+            y_pred = clf.predict(X_test)
+            accuracy = accuracy_score(y_test, y_pred)
+            results[imputation_name].append((clf_name, accuracy))
+            print("...{}".format(clf_name))
 
-    def _load_datasets(self, name):
-        pass
-
-    def run(self):
-        pass
+    with open(log_path, 'w') as f:
+        f.write(str(results))
+    print("Results saved to {}".format(log_path))
