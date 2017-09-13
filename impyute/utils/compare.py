99fdb5f5117db8ddc9a9d7a2dfccdde685036621
EltonLaw <eltonlaw296@gmail.com>
2017-09-13 00:51:12 -0400

    allow custom algorithms

--- impyute/utils/compare.py
+++ impyute/utils/compare.py
@@ -1,30 +1,50 @@
 """impyute.utils.compare.py"""
-from sklearn.svm import SVC
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.naive_bayes import GaussianNB
-from sklearn.neighbors import KNeighborsClassifier
+import importlib
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
-
 # pylint: disable=too-many-locals
-def compare(imputed, log_path="results.txt"):
-    """ Given an imputed dataset, runs a bunch of machine learning algorithms
+# pylint: disable=dangerous-default-value
+
+
+def compare(imputed, classifiers=["sklearn.svm.SVC"], log_path=None):
+    """
+    Given an imputed dataset with labels and a list of supervised machine
+    learning model, find accuracy score of all model/imputation pairs.
 
     Parameters
     ----------
     imputed: [(str, np.ndarray), (str, np.ndarray)...]
-       List of tuples containing (imputation algorithm name, imputed data)
+        List of tuples containing (imputation_name, imputed_data) where
+        `imputation_name` is a string and `imputed_data` is a tuple where
+        `imputed_data`[0] is the data, X and `imputed_data`[1] is the label, y
+    classifiers: [str, str...str] (optional)
+        Provide a list of classifiers to run imputed data sets on. Right now,
+        it ONLY works with sklearn, the format should be like so:
+        `sklearn.SUBMODULE.FUNCTION`. More generally its
+        'MODULE.SUBMODULE.FUNCTION'. If providing a custom classifier, make
+        sure to add the file location to sys.path first and the classifier
+        should also be structured like sklearn (with a `fit` and `predict`
+        method).
+    log_path: str (optional)
+        To write results to a file, provide a relative path
 
     Returns
-    ------
+    -------
     results.txt
         Classification results on imputed data
 
     """
-    clfs = [["SVC", SVC()],
-            ["KNeighbours", KNeighborsClassifier(2)],
-            ["GaussianNB", GaussianNB()],
-            ["RandomForestClassifier", RandomForestClassifier()]]
+    clfs = []
+    for clf_name in classifiers:
+        mod_name, smod_name, fn_name = clf_name.split(".")
+        try:
+            mod = importlib.import_module("{}.{}".format(mod_name, smod_name))
+            fn = getattr(mod, fn_name)
+            clfs.append([fn_name, fn])
+        except ModuleNotFoundError:
+            print("Cannot import '{}' from '{}.{}'".format(fn_name,
+                                                           mod_name,
+                                                           smod_name))
 
     results = {imputation_name: [] for imputation_name, _ in imputed}
 
@@ -41,6 +61,10 @@ def compare(imputed, log_path="results.txt"):
             results[imputation_name].append((clf_name, accuracy))
             print("...{}".format(clf_name))
 
-    with open(log_path, 'w') as f:
-        f.write(str(results))
-    print("Results saved to {}".format(log_path))
+    # If not None, write to path
+    if log_path:
+        with open(log_path, 'w') as f:
+            f.write(str(results))
+        print("Results saved to {}".format(log_path))
+
+    return results
