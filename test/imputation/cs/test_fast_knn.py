3da1fc6e4e839bfff80e1c58ceaec750d293abed
Elton Law <eltonlaw296@gmail.com>
2019-07-01 03:21:22 -0400

    [#53] unit test to make sure that weights are closer to 8 than 12

--- test/imputation/cs/test_fast_knn.py
+++ test/imputation/cs/test_fast_knn.py
@@ -4,7 +4,6 @@ import numpy as np
 import impyute as impy
 # pylint:disable=invalid-name
 
-
 class TestFastKNN(unittest.TestCase):
     """ Tests for Fast KNN """
     def setUp(self):
@@ -28,6 +27,14 @@ class TestFastKNN(unittest.TestCase):
         imputed = impy.fast_knn(self.data_m)
         self.assertFalse(np.isnan(imputed).any())
 
+    def test_impute_value(self):
+        data = np.array([[ 0. , 1. , np.nan, 3. , 4. ],
+                         [ 5. , 6. , 7. , 8. , 9. ],
+                         [10. , 11. , 12. , 13. , 14. ],
+                         [15. , 16. , 17. , 18. , 19. ],
+                         [20. , 21. , 22. , 23. , 24. ]])
+        imputed = impy.fast_knn(data, k=2) # Weighted average of nearest 2 neighbours
+        self.assertTrue(np.isclose(imputed[0][2], 8.913911092686593))
 
 if __name__ == "__main__":
     unittest.main()
