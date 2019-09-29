aacca91b7d8c706fdeff7eb191b12cb3805d3d05
Elton Law <elton.law@kirasystems.com>
2019-09-29 16:23:08 -0400

    [GH-90] init impyute.util.matrix for matrix ops

---
+++ test/util/test_matrix.py
@@ -0,0 +1,17 @@
+import numpy as np
+from impyute.util import matrix
+
+def _is_gt_5(x):
+    return x > 5
+
+def test_map_nd_2d():
+    arr = np.arange(10).reshape([5, 2])
+    expected = np.array([
+        [False, False],
+        [False, False],
+        [False, False],
+        [True, True],
+        [True, True],
+    ])
+    actual = map_nd(_is_gt_5, arr)
+    assert matrix.every_nd(bool, expected == actual)
