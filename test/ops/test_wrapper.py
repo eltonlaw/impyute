668b3d3bf5cee0b82f6da7f6f8ef1d0ac3f3fcec
Elton Law <elton.law@kirasystems.com>
2019-09-30 03:03:17 -0400

    [GH-90] - cleanup dead tests

--- test/ops/test_wrapper.py
+++ test/ops/test_wrapper.py
@@ -18,50 +18,26 @@ except ModuleNotFoundError:
     pass
 
 @preprocess
-def preprocess_mul(arr, **kwargs):
+def preprocess_mul(arr):
     """Some function that performs an inplace operation on the input. Accepts kwargs"""
     arr *= 25
     return arr
 
-@preprocess
-def preprocess_mul_no_kwargs(arr):
-    """Some function that performs an inplace operation on the input"""
-    arr *= 25
-    return arr
-
-def test_inplace_false():
+def test_preprocess_inplace_false():
     """Input should be unchanged if inplace set to false"""
     A = np.ones((5, 5))
     A_copy = A.copy()
     preprocess_mul(A, inplace=False)
     assert A[0, 0] == A_copy[0, 0]
 
-def test_inplace_true():
+def test_preprocess_inplace_true():
     """Input may be changed if inplace set to true and operation is inplace"""
     A = np.ones((5, 5))
     A_copy = A.copy()
     preprocess_mul(A, inplace=True)
     assert A[0, 0] != A_copy[0, 0]
 
-def test_inplace_false_nokwargs():
-    """Test that passed in function doesn't need to set kwargs as parameters
-    Input should be unchanged if inplace set to false
-    """
-    A = np.ones((5, 5))
-    A_copy = A.copy()
-    preprocess_mul_no_kwargs(A, inplace=False)
-    assert A[0, 0] == A_copy[0, 0]
-
-def test_inplace_true_nokwargs():
-    """Test that passed in function doesn't need to set kwargs as parameters
-    Input may be changed if inplace set to true and operation is inplace
-    """
-    A = np.ones((5, 5))
-    A_copy = A.copy()
-    preprocess_mul_no_kwargs(A, inplace=True)
-    assert A[0, 0] != A_copy[0, 0]
-
-def test_pandas_input():
+def test_preprocess_pandas_input():
     """ Input: DataFrame, Output: DataFrame """
     # Skip this test if you don't have pandas
     pytest.importorskip('pandas')
